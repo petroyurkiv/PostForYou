29a962ffe9eba47ca447f26c7371ac805bdb6a44
petroyurkiv <igorlamativ@gmail.com>
2023-06-13 16:49:46 +0300

    searchBar was added

---
+++ PostForYou/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  PostForYou
+//
+//  Created by Petro on 13.06.2023.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    private let bar = UIView()
+    private let searchBar = TextField()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureBarView()
+        configureSearchBar()
+    }
+    
+    private func configureBarView() {
+        view.addSubview(bar)
+        bar.backgroundColor = .systemGray2
+        bar.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 46)
+        bar.center = view.center
+    }
+
+    private func configureSearchBar() {
+        view.addSubview(searchBar)
+        searchBar.frame = CGRect(x: 0, y: 0, width: view.frame.width - 32, height: 36)
+        searchBar.center = view.center
+        searchBar.backgroundColor = .gray
+    }
+}
+
---
+++ PostForYou/TextField.swift
@@ -0,0 +1,41 @@
+//
+//  TextField.swift
+//  PostForYou
+//
+//  Created by Petro on 13.06.2023.
+//
+
+import UIKit
+
+class TextField: UITextField {
+    
+    private let padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 40)
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        confTextField()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func confTextField() {
+        placeholder = "Search"
+        textColor = .gray
+        layer.cornerRadius = 18
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.inset(by: padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.inset(by: padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.inset(by: padding)
+    }
+}
